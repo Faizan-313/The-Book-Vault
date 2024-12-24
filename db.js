3b5c49fd0a55b6ad49d764a0e24fe69b011a5eed
Faizan-313 <faizansyedpeer@gmail.com>
2024-12-25 00:16:12 +0530

    changed database added login table

---
+++ db.js
@@ -0,0 +1,23 @@
+import pg from "pg";
+import env from "dotenv";
+
+env.config();
+
+//setup the postgres database
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
+
+db.connect(err=>{
+    if(err){
+        console.log('Database error',err);
+    }else{
+        console.log('Database connected');
+    }
+});
+
+export default db;
