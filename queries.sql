9ef043d6ec1247512b5c8ddb3b646d3396968107
Faizan-313 <faizansyedpeer@gmail.com>
2024-12-25 16:11:01 +0530

    change some tables

--- queries.sql
+++ queries.sql
@@ -1,10 +1,25 @@
-CREATE TABLE books(
-	id SERIAL PRIMARY KEY NOT NULL,
-	name VARCHAR(200) UNIQUE NOT NULL,
-	rattings REAL NOT NUll,
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL
+);
+
+CREATE TABLE books (
+    book_id SERIAL PRIMARY KEY,
+    book_name VARCHAR(200) UNIQUE NOT NULL,
+    cover_url TEXT,
+    average_rating REAL,
+    author VARCHAR(200)
+);
+
+CREATE TABLE user_saved (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    book_id INT NOT NULL,
+    user_rating real not null,
+    book_description text,
     event_date date,
-    cover_url text,
-    average_ratting real,
-    author VARCHAR(200),
-    description text
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE
 );
