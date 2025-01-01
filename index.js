d19bab60493f0638993a8ede1d3dc751358bc2f5
Faizan-313 <faizansyedpeer@gmail.com>
2025-01-01 10:55:35 +0530

    typo

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use(session({
     saveUninitialized: false,
     name: 'sessionId',
     cookie: {
-        secure: false,
+        secure: true,
         httpOnly: true,
         maxAge: 1000 * 60 * 30, 
     }
